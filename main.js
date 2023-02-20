47398196e1b8d3fcf833dbb942d5c92c0c3d0841
minhphucdev201 <quockhanh170901@gmail.com>
2023-02-20 16:48:34 +0700

    add your commit

---
+++ main.js
@@ -0,0 +1,10 @@
+import "./style.css";
+import javascriptLogo from "./javascript.svg";
+
+document.querySelector("#app").innerHTML = `
+  <div>
+       <p class="text-2xl">hihi</p>
+
+   
+  </div>
+`;
